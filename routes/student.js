df53a744eb1d3ab789306c8c02708494208d7252
Harikamadamanchi <harikamadamanchi13@gmail.com>
2022-11-16 23:56:33 -0600

    delete and update

--- routes/student.js
+++ routes/student.js
@@ -5,6 +5,12 @@ const Student_controllers = require('../controllers/Student');
 /* GET home page. */
 router.get('/',Student_controllers.Student_view_all_Page);
 /* GET detail costume page */ 
-router.get('/detail', Student_controllers.Student_view_one_Page); 
+router.get('/detail', Student_controllers.Student_view_one_Page);
+/* GET create costume page */ 
+router.get('/create', Student_controllers.Student_create_Page);
+/* GET create update page */ 
+router.get('/update', Student_controllers.Student_update_Page);
+/* GET delete costume page */ 
+router.get('/delete', Student_controllers.Student_delete_Page); 
 
 module.exports = router;
