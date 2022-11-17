3010c13abcdb68f2fb325edd8201fb9e673a45de
Harikamadamanchi <harikamadamanchi13@gmail.com>
2022-11-16 23:22:07 -0600

    details

--- routes/student.js
+++ routes/student.js
@@ -4,5 +4,7 @@ var router = express.Router();
 const Student_controllers = require('../controllers/Student');
 /* GET home page. */
 router.get('/',Student_controllers.Student_view_all_Page);
+/* GET detail costume page */ 
+router.get('/detail', Student_controllers.Student_view_one_Page); 
 
 module.exports = router;
